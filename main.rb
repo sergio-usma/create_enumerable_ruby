12af73db9740f91088006b00dae526089a6555f5
Ali Baba <ali7090370@gmail.com>
2023-08-26 00:48:56 +0430

    Testing MyList class

--- main.rb
+++ main.rb
@@ -11,3 +11,10 @@ class MyList
     @list.each
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
